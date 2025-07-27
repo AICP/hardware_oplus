LineageOS/android_hardware_oplus

interfaces: Add vendor.oplus.hardware.esim V1
Change-Id: I7cbbdc8910073261b3f07603b3dc012de3fdbfc0

interfaces/aidl/vendor/oplus/hardware/esim/IOplusEsim.aidl
@@ -1,0 +1,14 @@
+/*
+ * SPDX-FileCopyrightText: 2025 The LineageOS Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package vendor.oplus.hardware.esim;
+
+@VintfStability
+interface IOplusEsim {
+    int getEsimGpio();
+    int setEsimGpio(int gpio);
+    int setHotswap();
+    int setUimPower(int operation);
+}

interfaces/aidl_api/vendor.oplus.hardware.esim/1/vendor/oplus/hardware/esim/IOplusEsim.aidl
@@ -1,0 +1,30 @@
+/*
+ * SPDX-FileCopyrightText: 2025 The LineageOS Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+///////////////////////////////////////////////////////////////////////////////
+// THIS FILE IS IMMUTABLE. DO NOT EDIT IN ANY CASE.                          //
+///////////////////////////////////////////////////////////////////////////////
+
+// This file is a snapshot of an AIDL file. Do not edit it manually. There are
+// two cases:
+// 1). this is a frozen version file - do not edit this in any case.
+// 2). this is a 'current' file. If you make a backwards compatible change to
+//     the interface (from the latest frozen version), the build system will
+//     prompt you to update this file with `m <name>-update-api`.
+//
+// You must not make a backward incompatible change to any AIDL file built
+// with the aidl_interface module type with versions property set. The module
+// type is used to build AIDL files in a way that they can be used across
+// independently updatable components of the system. If a device is shipped
+// with such a backward incompatible change, it has a high risk of breaking
+// later when a module using the interface is updated, e.g., Mainline modules.
+
+package vendor.oplus.hardware.esim;
+@VintfStability
+interface IOplusEsim {
+  int getEsimGpio();
+  int setEsimGpio(int gpio);
+  int setHotswap();
+  int setUimPower(int operation);
+}

AICP/hardware_oplus

interfaces/aidl/vendor/oplus/hardware/esim/IOplusEsim.aidl
@@ -1,0 +1,15 @@
+/*
+ * SPDX-FileCopyrightText: 2025 The LineageOS Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package vendor.oplus.hardware.esim;
+
+@VintfStability
+interface IOplusEsim {
+    int getEsimGpio();
+    void initEsimGpio();
+    int setEsimGpio(int operation);
+    int setHotswap();
+    int setUimPower(int operation);
+}

interfaces/aidl_api/vendor.oplus.hardware.esim/1/vendor/oplus/hardware/esim/IOplusEsim.aidl
@@ -1,0 +1,31 @@
+/*
+ * SPDX-FileCopyrightText: 2025 The LineageOS Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+///////////////////////////////////////////////////////////////////////////////
+// THIS FILE IS IMMUTABLE. DO NOT EDIT IN ANY CASE.                          //
+///////////////////////////////////////////////////////////////////////////////
+
+// This file is a snapshot of an AIDL file. Do not edit it manually. There are
+// two cases:
+// 1). this is a frozen version file - do not edit this in any case.
+// 2). this is a 'current' file. If you make a backwards compatible change to
+//     the interface (from the latest frozen version), the build system will
+//     prompt you to update this file with `m <name>-update-api`.
+//
+// You must not make a backward incompatible change to any AIDL file built
+// with the aidl_interface module type with versions property set. The module
+// type is used to build AIDL files in a way that they can be used across
+// independently updatable components of the system. If a device is shipped
+// with such a backward incompatible change, it has a high risk of breaking
+// later when a module using the interface is updated, e.g., Mainline modules.
+
+package vendor.oplus.hardware.esim;
+@VintfStability
+interface IOplusEsim {
+  int getEsimGpio();
+  void initEsimGpio();
+  int setEsimGpio(int operation);
+  int setHotswap();
+  int setUimPower(int operation);
+}